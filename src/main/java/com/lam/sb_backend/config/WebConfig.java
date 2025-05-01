0c00c60ea7081f597178d0a7903739ac8bdca01e
lzruiv4 <lzruiv4@gmail.com>
2025-05-01 16:52:35 +0200

    20250430 :: update db

--- src/main/java/com/lam/sb_backend/config/WebConfig.java
+++ src/main/java/com/lam/sb_backend/config/WebConfig.java
@@ -5,11 +5,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class AngularWebConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins(
+                        "http://localhost:4200",
+                        "http://localhost:9000"
+                )
                 .allowedMethods("*");
     }
 }
